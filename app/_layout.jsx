c08314c2cb2cf0e809f834f41ea75251c43e8caf
Souvik <souviksarkar260123@gmail.com>
2024-10-02 12:30:14 +0530

    routing

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Slot, Stack } from "expo-router";
+import { Text } from "react-native";
 
-export default function App() {
+const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text style={{fontSize: 50}}>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <>
+    //   <Text>Header</Text>
+    //   <Slot />
+    //   <Text>Footer</Text>
+    // </>
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false }}
+      ></Stack.Screen>
+    </Stack>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default RootLayout;
---
+++ app/index.jsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 40 }}>Hello world</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile">Go to Profile</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
