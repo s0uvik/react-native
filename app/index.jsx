574a30127ac736a5754387f14df0415325f4bea0
Souvik <souviksarkar260123@gmail.com>
2024-10-02 18:08:02 +0530

    add fonts

--- app/index.jsx
+++ app/index.jsx
@@ -5,18 +5,9 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex flex-1 items-center justify-center bg-white">
-      <Text className=" text-3xl">Hello world!</Text>
+      <Text className=" text-3xl font-pblack">Hello world!</Text>
       <StatusBar style="auto" />
       <Link href="/profile">Go to Profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
