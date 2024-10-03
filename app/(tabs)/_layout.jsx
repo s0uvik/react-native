992dbb5f9f1f6a8ad79d4ddfe8323bdc53f223e2
Souvik <souviksarkar260123@gmail.com>
2024-10-03 11:57:17 +0530

    create tabs screen

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Tabs, Redirect } from "expo-router";
 
-const TabsLayout = () => {
+import { icons } from "../../constants";
+
+const TabIcon = ({ icon, color, name, focused }) => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
+    <View className=" items-center justify-center">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? " font-psemibold" : " font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
     </View>
   );
 };
 
-export default TabsLayout;
+const tabs = ["home", "bookmark", "create", "profile"];
 
-const styles = StyleSheet.create({});
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 60,
+          },
+        }}
+      >
+        {tabs.map((item) => {
+          const capItem = item.charAt(0).toUpperCase() + item.slice(1);
+          return (
+            <Tabs.Screen
+              key={item}
+              name={item}
+              options={{
+                title: capItem,
+                headerShown: false,
+                tabBarIcon: ({ color, focused }) => (
+                  <TabIcon
+                    icon={item === "create" ? icons.plus : icons[item]}
+                    color={color}
+                    name={capItem}
+                    focused={focused}
+                  />
+                ),
+              }}
+            />
+          );
+        })}
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
