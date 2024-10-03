f78a352b51104b261d9a675664b02f06346d9234
Souvik <souviksarkar260123@gmail.com>
2024-10-03 15:01:18 +0530

    create sign up page

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,78 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {
+    return;
+  };
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className=" bg-primary h-full">
+      <ScrollView>
+        <View className=" w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className=" w-[115px] h-[35px]"
+          />
+          <Text className=" text-2xl text-white text-semibold mt-10 font-psemibold">
+            Sign up to Aora
+          </Text>
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyle="mt-7"
+            placeholder="Enter username"
+          />
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyle="mt-7"
+            keyboardType="email-address"
+            placeholder="Enter email"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyle="mt-7"
+            placeholder="Password"
+          />
+
+          <CustomButton
+            title="Sign up"
+            containerStyle="mt-7"
+            handlePress={submit}
+            isLoading={isSubmitting}
+          />
+          <View className=" justify-center pt-5 flex-row gap-2">
+            <Text className=" text-md text-gray-100 font-pregular">
+              Have an account?
+            </Text>
+            <Link
+              className="text-md text-secondary font-psemibold"
+              href="/sign-in"
+            >
+              Sign in
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
